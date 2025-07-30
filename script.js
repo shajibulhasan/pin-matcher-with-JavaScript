405d63d2ad9c1afa9a2185383b48ff114dfec1ff
shajibulhasan <shajibulhasan.puc@gmail.com>
2025-07-30 22:57:44 +0600

    tries control added

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ const showPin = document.getElementById("showPin");
 document.getElementById("pinGenerate").addEventListener("click", function() {
     let pin = Math.floor(1000 + Math.random() * 9000);
     showPin.value = pin;
+    timeLeft.innerText = "3";
+    notMatch.style.display = "none";
+    match.style.display = "none";
+    display.value = "";
+    tries = 3;
+    document.getElementById("submit").disabled = false;
 });
 
 const button = document.getElementsByClassName("button");
@@ -27,6 +33,8 @@ for (let i = 0; i < button.length; i++) {
         });
     }
 }
+const timeLeft = document.getElementById("time");
+let tries = 3;
 document.getElementById("submit").addEventListener("click", function() {
     const pin = showPin.value;
     const userInput = display.value;
@@ -36,5 +44,12 @@ document.getElementById("submit").addEventListener("click", function() {
     } else {
         notMatch.style.display = "block";
         match.style.display = "none";
+        tries--;
+        timeLeft.innerText = tries;
+        if (tries <= 0) {
+            document.getElementById("submit").disabled = true;
+            timeLeft.innerText = "No tries left";
+            notMatch.innerText = "❌ No tries left, please generate a new pin";
+        }
     }
 });
